c62fa63292f75ef457eaf40a9a0280c0ad3e5c0f
loic ortola <contact@loicortola.com>
2018-03-04 22:48:14 +0100

    Added default layer

--- conf/conf.js
+++ conf/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
   "defaultLayers": {
-  "jawg-vector": { "label": "Jawg Streets - Vector", "type": "proxy", "vector": true, "source": "https://tile.jawg.io/jawg-streets.json?access-token=community" },
+  "jawg-vector": { "default": true, "label": "Jawg Streets - Vector", "type": "proxy", "vector": true, "source": "https://tile.jawg.io/jawg-streets.json?access-token=community" },
   "jawg": { "label": "Jawg Streets", "type": "proxy", "retina": true, "source": "http://tile.jawg.io/jawg-streets/{z}/{x}/{y}@2x.png?access-token=community" },
   "osm": {"label": "OpenStreetMap", "type": "proxy", "retina": false, "source": "http://tile.openstreetmap.org/{z}/{x}/{y}.png"},
   "hot": {"label": "OpenStreetMap HOT", "type": "proxy", "retina": false, "source": "http://tile-a.openstreetmap.fr/hot/{z}/{x}/{y}.png"}
@@ -8,5 +8,5 @@ module.exports = {
   "dataPath": "tiles",
   "assetsDataPath": "assets",
   "protocol": "http",
-  "host": "localhost:3000"
+  "host": "localhost:8081"
 };
